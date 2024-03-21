36868dc4a087747c447c976fdbf92ea8094eebbc
Moté <81959473+PetitMote@users.noreply.github.com>
2024-03-21 21:39:10 +0100

    Add text search config (#8)
    * Added text search parameter
    
    * Use of custom text search lang
    
    The db_search request now makes use of the text search lang config.

--- config_sample.py
+++ config_sample.py
@@ -1,9 +1,12 @@
-SECRET_KEY = '<KEY>' # As for now, OokCatalog does not make use of any cookie
+SECRET_KEY = "<KEY>"  # As for now, OokCatalog does not make use of any cookie
 DATABASE = {
-            "HOST": "localhost",
-            "PORT": "5432",
-            "DBNAME": "ookcatalog",
-            "USER": "ookcatalog",
-            "PASSWORD": "password",
-        }
+    "HOST": "localhost",
+    "PORT": "5432",
+    "DBNAME": "ookcatalog",
+    "USER": "ookcatalog",
+    "PASSWORD": "password",
+}
 DATABASE_TITLE = "BDU"
+# Language passed to PostgreSQL for full text search. Needed so it knows how to interpret both the search requests and
+# the documents.
+TEXT_SEARCH_LANG = "french"
